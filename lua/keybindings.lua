08a4ee301efb1d25c496fbcba6ae5072f31059da
Christopher James <bvlyqt@gmail.com>
2023-01-25 15:15:39 +0900

    Added a dedicated file for highlights customisation, but this is a bit of a hack, and I really need to start from the ground up here. Plug is nice and simple, but it might be worth trying alternatives, like Lazy. That being said, if there isn't a perf. benefit then there's probably no reason to besides cleanliness.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,26 +1,24 @@
--- Keybindings
--- ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+-- KEYBINDINGS
 
--- local map = vim.api.nvim_set_keymap
--- map('i', '<c-j>', '<esc>')
--- map('n', 'J', '}')
+-- -----------------------------------------------------------------------------
 
--- function mapper(mode, lhs, rhs, opts)
---     local options = {noremap = true}
---     if opts then
---         options = vim.tbl_extend("force", options, opts)
---     end
---     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
--- end
--- mapper("n", ";f", ":Telescope find_files<CR>", {silent = true})
+-- `opts` Helpers
+-- `noremap`: Ensures that your mapping will always take precedence over any other attempts to remap it. Note: this only applies to the mode defined in the mapping.
+-- `silent`: This tells Vim to not display any error messages if the key mapping fails.
+-- `expr`: This tells Vim to execute the right-hand side of the mapping as an expression. This means that the right-hand side can be any valid Vimscript expression, and it will be evaluated and executed when the key sequence is pressed. *Todo This is useful for mapping keys to functions that return a string.
+-- `replace_keycodes`: This tells Vim to not replace key codes in the key sequence with the corresponding characters. This can be useful in situations where you want to map a key sequence that includes special key codes, such as <C-a> for Control+a.
 
--- ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+-- -----------------------------------------------------------------------------
 
--- Helpers
--- ―――――――
+-- Helper Functions
 
-function map(mode, new, old)
-    vim.api.nvim_set_keymap(mode, new, old, {noremap=true, silent=true})
+function map(mode, new, old, opts)
+    -- map("n", ";f", ":Telescope find_files<CR>", {expr = true})
+    local default_opts = {}
+    if opts then
+        options = vim.tbl_extend("force", default_opts, opts) -- Merges the `default_opts` and `opts` tables
+    end
+    vim.api.nvim_set_keymap(mode, new, old, options)
 end
 
 function insert(new, old)
@@ -35,12 +33,11 @@ function visual(new, old)
     vim.api.nvim_set_keymap('v', new, old, {noremap=true, silent=true})
 end
 
--- ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+-- -----------------------------------------------------------------------------
 
 if (vim.g.vscode) then
 
-    -- Visual Studio Code
-    -- ―――――――――――――――――――――――――――――――――
+    -- VISUAL STUDIO CODE
     
     -- TODO: Leader key doesn't seem to work in Code.
     -- vim.g.mapleader = ','
@@ -61,46 +58,53 @@ if (vim.g.vscode) then
 
 else
 
-    -- Neovim
-    
+    -- NEOVIM
+
     -- Leader Key
     vim.g.mapleader = ' ' -- `vim.g.mapleader = '<Space>'` doesn't work.
 
-    -- Remap Esc Key
+    -- Esc Key
     -- normal('<C-j>', '<esc>')
     -- visual('<C-j>', '<esc>')
     -- insert('<C-j>', '<esc>')
+    -- normal('<leader-o>', 'o<Esc>^D0')
 
     normal('J', '}') -- Jump paragraph backwards in normal mode.
     normal('K', '{') -- Jump paragraph forwards in normal mode.
     normal('L', '$') -- Jump to the end of the active line.
     normal('H', '_') -- Jump to the beginning of the active line.
     normal('<n>', '<C-v>') -- WIN: Fixes paste overlap w/ Visual Command Mode.
-
     visual('J', '}') -- Jump paragraph backwards in visual mode.
     visual('K', '{') -- Jump paragraph forwards in visual mode.
+    visual('p', 'pgvy') -- Paste, reselect the original selection with `gv`, and re-yank the selection.
     
-    -- TODO What's the difference between these?
-    normal('<leader>y', '"+y') -- Copy to Clipboard
-    visual('<leader>y', '"+y') -- Copy to Clipboard
-    -- normal('<leader>yy', '"+yy') -- Copy the active line to the clipboard.
+    -- Copy to System Clipboard
+    normal('<leader>y', '"+y') -- Normal Mode: Copy to clipboard
+    normal('<leader>yy', '"+yy') -- Normal Mode: Copy the active line to the clipboard.
+    normal('<leader>Y', '"+Y') -- Normal Mode: Copy the active line to the clipboard starting from the cursor.
+    visual('<leader>y', '"+y') -- Visual Mode: Copy to clipboard
 
-    normal('<leader>p', '"+p') -- Paste from Clipboard
-    normal('<leader>P', '"+P') -- Paste from Clipboard
-    visual('<leader>p', '"+p') -- Paste from Clipboard
-    visual('<leader>P', '"+P') -- Paste from Clipboard
-
-    -- OLS Formatter
-    -- -------------------------------------------------------------------------
-    -- au BufNewFile,BufRead *.odin map <C-P> :w<Return>:%!odinfmt %<Return>
-    vim.cmd [[ au BufNewFile,BufRead *.odin map=<C-P> :w<Return>:%!odinfmt %<Return> ]]
+    -- Paste from System Clipboard
+    normal('<leader>p', '"+p') -- Normal Mode: Paste from clipboard
+    normal('<leader>P', '"+P') -- Normal Mode: Paste from clipboard
+    visual('<leader>p', '"+p') -- Visual Mode: Paste from clipboard
+    visual('<leader>P', '"+P') -- Visual Mode: Paste from clipboard
 
     -- CoC
-    -- -------------------------------------------------------------------------
+    function _G.check_back_space()
+        local col = vim.fn.col('.') - 1
+        return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+    end
+
+    local coc_opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+
+    -- Tab Completion
+    -- This seems irrelevant... I can just space out of the selection...
+    map('i', '<CR>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], coc_opts)
+
     normal('gd', '<Plug>(coc-definition)')
     normal('[g', '<Plug>(coc-diagnostic-prev)')
     normal(']g', '<Plug>(coc-diagnostic-next)')
-    insert('<c-space>', 'coc#refresh()')
 
     function _G.show_docs()
         local cw = vim.fn.expand('<cword>')
@@ -112,30 +116,40 @@ else
             vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
         end
     end
+
     normal('<C-k>', '<cmd>lua show_docs()<CR>')
 
     -- Leap
-    -- -------------------------------------------------------------------------
+    -- Leap is a died-in-the-wool never-x-er so I'm deleting x here so that Leap's
+    -- overriding of x doesn't get in the way of my x-ing.
+    vim.keymap.del({'x', 'o'}, 'x')
+    vim.keymap.del({'x', 'o'}, 'X')
     visual('g', '<Plug>(leap-forward-till)') -- Fixes leap's remapping of x in visual mode.
 
+    -- ols Formatting
+    -- au BufNewFile,BufRead *.odin map <C-P> :w<Return>:%!odinfmt %<Return>
+    vim.cmd [[ au BufNewFile,BufRead *.odin map=<C-'> :w<Return>:%!odinfmt %<Return> ]]
+
+    -- nvim-tree.lua
+    normal('<C-h>', ':NvimTreeToggle<cr>')
+
+    -- TO-DO
+
     -- HighStr
-    -- -------------------------------------------------------------------------
     -- visual('<F3>', ':<c-u>HSHighlight 1<CR>')
     -- visual('<F4>', ':<c-u>HSRmHighlight 1<CR>')
 
     -- Trouble
-    -- -------------------------------------------------------------------------
     normal('<leader>-', '<cmd>Trouble<CR>')
-    -- normal('<leader>xw', '<cmd>Trouble workspace_diagnostics<CR>')
-    -- normal('<leader>xd', '<cmd>Trouble document_diagnostics<CR>')
-    -- normal('<leader>xl', '<cmd>Trouble loclist<CR>')
-    -- TODO normal('<leader>... '<cmd>Trouble quickfix<CR>')
-    -- TODO normal('<leader>... '<cmd>Trouble lsp_reference<CR>')
+    normal('<leader>xw', '<cmd>Trouble workspace_diagnostics<CR>')
+    normal('<leader>xd', '<cmd>Trouble document_diagnostics<CR>')
+    normal('<leader>xl', '<cmd>Trouble loclist<CR>')
+    -- TO-DO normal('<leader>... '<cmd>Trouble quickfix<CR>')
+    -- TO-DO normal('<leader>... '<cmd>Trouble lsp_reference<CR>')
 
     -- Toggle Term
-    -- -------------------------------------------------------------------------
-    -- TODO Input mode not working as desired.
-    -- normal('C-\\', '')
+    -- TO-DO Input mode not working as desired.
+    -- no-rmal('C-\\', '')
     -- function _G.set_terminal_keymaps()
     --     local opts = {buffer = 0}
     --     vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
@@ -147,7 +161,6 @@ else
     -- end
 
     -- Telescope
-    -- ―――――――――――――――――――――――――――――――――    
     -- normal(';f', ':Telescope find_files<CR>')
     -- normal(';g', ':Telescope live_grep<CR>')
     -- normal(';b', ':Telescope buffers<CR>')
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,5 @@
--- An init.lua example:
+-- Notes
 -- https://github.com/mukeshsoni/config/blob/master/.config/nvim/init.lua
-
 -- Echo Neovim's runtimepath variables.
 -- print(vim.inspect(vim.api.nvim_list_runtime_paths()))
 
@@ -9,21 +8,17 @@ if (vim.g.vscode) then
     -- Visual Studio Code Settings
 
     vim.cmd([[ let g:qs_highlight_on_keys = ['f', 'F'] ]])
-
     -- Colours based on: https://icolorpalette.com/color/pantone-16-0836-tcx
-    -- vim.cmd([[
-    --     highlight QuickScopePrimary guifg='#c8b273'
-    --     highlight QuickScopeSecondary guifg='#73c7b2'
-    -- ]])
+    vim.cmd([[
+        highlight QuickScopePrimary guifg='#c8b273'
+        highlight QuickScopeSecondary guifg='#73c7b2'
+    ]])
 
 else
 
     -- Neovim Settings
 
-    -- Global Options
-
     -- Netrw
-
     vim.g.loaded_netrw = 1 -- netrw is Neovim's built in file explorer
     vim.g.loaded_netrwPlugin = 1
     -- vim.g.netrw_banner = 0
@@ -31,35 +26,37 @@ else
     -- vim.g.netrw_browse_split = 3 -- Open new files from netrw in a new tab.
     -- vim.g.netrw_winsize = 20
 
-    -- Options
-
+    -- Misc
     vim.o.encoding = 'utf-8'
     vim.o.termguicolors = true -- Enable true 24bit colour support; required for custom Neovim theme.
     vim.o.background = 'dark'
-
     vim.o.hidden = true -- Retain undo information when a buffer is unloaded (incl. ToggleTerm)
-    vim.o.showcmd = false -- Show input in the status line.
 
-    vim.o.showmatch = true -- Show matching braces, etc.
+    -- Syntax
     vim.o.ignorecase = true -- Ignore case when searching.
     vim.o.smartcase = true -- Don't ignore case with capitals.
     vim.o.hlsearch = true -- Highlight search results.
-    vim.o.incsearch = true -- Show search results as you type.
-    vim.o.synmaxcol = 10000 -- Max columns (letters) to search for syntax items.
+    vim.o.synmaxcol = 20000 -- Max columns (letters) to search for syntax items.
 
+    -- UI
+    vim.o.showcmd = true -- Show input in the status line.
+    vim.o.incsearch = true -- Show search results as you type.
+    vim.o.showmatch = true -- Show matching braces, etc.
     vim.o.syntax = 'on' -- Enable syntax highlighting.
     vim.o.filetype = 'on'
+
+    vim.o.laststatus = 3 -- 2 ensures that all windows have a status line. 3 enables the global status line. -- This needs to be called before fillchars...
     vim.o.fillchars = 'eob: ' -- Remove empty buffer symbols.
-    vim.o.foldenable = false -- Make sure folds are open by default.
-    vim.o.laststatus = 2 -- Ensures that all windows have a status line.
     vim.o.number = false -- Hide line numbers.
     vim.o.relativenumber = false 
     vim.o.signcolumn = 'no' -- Force the signcolumn to remain hidden.
 
+    -- Lines
     vim.o.linebreak = true -- Wrap text on word boundaries.
     vim.o.list = false
     vim.o.wrap = false -- Wrap lines. 
 
+    -- Indentation
     vim.o.autoindent = true
     vim.o.smartindent = true
     vim.o.breakindent = true -- Indent wrapped lines.
@@ -67,24 +64,34 @@ else
     -- vim.o.showbreak = '␤'
     -- vim.o.breakindentopt = 'shift:1'
 
+    -- Tabs
     vim.o.tabstop = 4
     vim.o.expandtab = true
     vim.o.smarttab = true
 
+    -- Timings
     vim.o.updatetime = 150 -- Length of time Vim waits after you stop typing before it triggers plugin.
-    vim.o.ttimeoutlen = 50 -- Timeout in ms for key codes.
+    vim.o.ttimeoutlen = 30 -- Timeout in ms for key codes.
+    vim.o.nottimeout = true
+    vim.o.redrawtime = 100
     -- vim.o.shadafile = 'NONE' -- Doesn't work?
     -- vim.o.lazyredraw = true -- Tried for perf.
 
-    vim.o.foldmethod = 'expr'
+    -- Folds
+    vim.o.foldenable = true -- Make sure folds are open by default.
+    vim.o.foldmethod = 'syntax'
     vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.o.foldlevel = 99
+
     vim.o.inccommand = 'nosplit' --Shows the effects of a command incrementally in the buffer.
 
     -- Window Options
-
     vim.w.foldcolumn = 1
 
-    --- Hacks
+    -- Hacks
+    -- Coc specific flags; 'Some servers have issues with backup files'
+    vim.opt.backup = false
+    vim.opt.writebackup = false
 
     vim.diagnostic.config({underline = false})
     -- vim.wo.conceallevel = 1
@@ -100,16 +107,21 @@ else
         end,
     })
 
+    -- Vim Commands
     vim.cmd [[ set mouse=a ]] -- Enables mouse scrolling.
     vim.cmd [[ set undofile ]] -- Keep undo history between sessions.
-    -- vim.cmd [[ let g:dracula_underline = 0 ]] -- Disable underlines in Dracula theme.
+    vim.cmd [[ autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o ]] -- Disable auto comment formatting. (`c`: auto-wrap comments; `r`: auto-insert comment leader after hitting <Enter> in Insert mode; `o`: Auto-insert the comment leader after hitting 'o' or 'O' in Normal mode.
     vim.cmd [[ let g:qs_highlight_on_keys = ['f', 'F'] ]] -- Highlight search terms on 'f' and 'F' keypresses.
-    vim.cmd [[ highlight QuickScopePrimary guifg='#c82491' gui=bold ctermfg=178 cterm=bold ]] -- Set QuickScope highlight colours.
-    vim.cmd [[ highlight QuickScopeSecondary guifg='#afff00' ctermfg=154 ]] -- Set QuickScope secondary highlight colours.
+    -- TODO 
+    vim.cmd [[ highlight Todo guifg=#FF9E3B guibg=bg gui=bold ctermfg=178 cterm=bold ]] -- Change highlighting for todo tags.
+    -- vim.cmd [[ let g:dracula_underline = 0 ]] -- Disable underlines in Dracula theme.
+    -- vim.cmd [[ highlight QuickScopePrimary guifg='#c82491' gui=bold ctermfg=178 cterm=bold ]] -- Set QuickScope highlight colours.
+    -- vim.cmd [[ highlight QuickScopeSecondary guifg='#afff00' ctermfg=154 ]] -- Set QuickScope secondary highlight colours.
+    vim.cmd [[ au BufNewFile,BufRead *.odin map=<C-P> :w<Return>:%!odinfmt %<Return> ]]
     -- vim.cmd [[ au BufNewFile,BufRead *.odin set syntax=odin ]] -- Set Odin syntax highlighting for .odin files.
     -- vim.cmd [[ au BufNewFile,BufRead *.odin set filetype=odin ]] -- Set Odin filetype for .odin files.
     -- vim.cmd [[ au BufNewFile,BufRead *.go set syntax=go ]]
     -- vim.cmd [[ au BufNewFile,BufRead *.go set filetype=go ]]
-    vim.cmd [[ au BufNewFile,BufRead *.odin map=<C-P> :w<Return>:%!odinfmt %<Return> ]]
 
 end
+
