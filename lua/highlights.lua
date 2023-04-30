2cc4a5e69bf79679bb0dc2d2cacddf82032b25e7
Christopher James <bvlyqt@gmail.com>
2023-04-30 15:19:44 +0900

    Removed the redundant tree-sitter-odin brace dimming highlights until I can figure out why that grammar isn't working.

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,31 +1,36 @@
 -- highlights.lua
--- -------------------------------------------------------------------------- --
 
-local fuji_white = "#DCD7BA"
+-- local fuji_white = "#DCD7BA"
 local sumi_ink_2 = "#2A2A37"
+
+-- TODO: What's up with the 'Undefined global variable `NONE`' warning?
+-- NOTE: It's so stupid that I have to do this.
+NONE = NONE
 
 vim.api.nvim_set_hl(0, "LineNr", {fg = sumi_ink_2, bg = NONE, gui = NONE})
 
 -- TODO Highlight doesn't apply when set with vim.api.nvim_set_hl...
+
 vim.cmd [[ highlight WinSeparator guifg=bg guibg=bg cterm=NONE ]]
 
-vim.api.nvim_set_hl(0, "@procOpeningBrace",          {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@procClosingBrace",          {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@nestedOpeningBrace",        {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@nestedClosingBrace",        {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@nestedIfTrueOpeningBrace",  {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@nestedIfTrueClosingBrace",  {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@nestedIfFalseOpeningBrace", {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@nestedIfFalseClosingBrace", {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@forStatementSemicolon",     {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@switchStatement",           {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@structOpeningBrace",        {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@structClosingBrace",        {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "myCodeBlockOpeningBrace",    {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "myCodeBlockClosingBrace",    {fg = sumi_ink_2})
+-- NOTE: tree-sitter-odin is a pain in the ass so opening and closing brace
+-- dimming are on hold for the time being.
 
--- TODO: Figure out a way to check which theme is currently active so that you can set the indent blankline colours to match dynamically.
+-- vim.api.nvim_set_hl(0, "@procOpeningBrace",          {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@procClosingBrace",          {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@nestedOpeningBrace",        {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@nestedClosingBrace",        {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@nestedIfTrueOpeningBrace",  {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@nestedIfTrueClosingBrace",  {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@nestedIfFalseOpeningBrace", {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@nestedIfFalseClosingBrace", {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@forStatementSemicolon",     {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@switchStatement",           {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@structOpeningBrace",        {fg = sumi_ink_2})
+-- vim.api.nvim_set_hl(0, "@structClosingBrace",        {fg = sumi_ink_2})
+
+-- TODO: Figure out a way to check which theme is currently active so that
+-- you can set the indent blankline colours to match dynamically.
 
 vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg = "#252535"}) -- Kanagawa
 -- vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg = "#252535"}) -- Code Dark
-
