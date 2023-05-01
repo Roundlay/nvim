8ced0644634b4598798f6060e7e7a229c1b28dfc
Christopher James <bvlyqt@gmail.com>
2023-05-01 19:16:51 +0900

    Updating benchmarks.

--- init.lua
+++ init.lua
@@ -4,17 +4,17 @@
 -- Benchmarks
 -- -------------------------------------------------------------------------- --
 
--- Lazy Benchmark (26-04-23)
+-- Lazy Benchmark (01-05-23)
 -- Environment: Windows 11 -> Alacritty (Administrator) -> Clink -> NVIM v0.9.0
 -- Command: `hyperfine --warmup 5 "nvim init.lua --headless +qa"`
--- Time:  (mean ± σ):    124.3 ms ±   1.1 ms   [User: 62.5 ms, System: 67.2 ms]
--- Range: (min … max):   123.1 ms … 127.9 ms   16 runs
+-- Time (mean ± σ):    106.4 ms ±   2.3 ms  [User: 56.7 ms, System: 70.5 ms]
+-- Range (min … max):  104.6 ms … 115.5 ms  20 runs
 
 -- Vim-Plug Benchmark (22-04-23)
 -- Environment: Windows 11 -> Alacritty (Administrator) -> Clink -> NVIM v0.9.0
 -- Command: `hyperfine --warmup 5 "nvim init.lua --headless +qa"`
--- Time (mean ± σ):   279.6 ms ±   6.2 ms    [User: 108.4 ms, System: 201.6 ms]
--- Range (min … max): 274.5 ms … 295.2 ms    10 runs
+-- Time (mean ± σ):    279.6 ms ±   6.2 ms  [User: 108.4 ms, System: 201.6 ms]
+-- Range (min … max):  274.5 ms … 295.2 ms  10 runs
 
 -- Initialisation
 -- -------------------------------------------------------------------------- --
