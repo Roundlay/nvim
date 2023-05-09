4bc4241cebfa077e8adfd51a2038b07f18c78526
Christopher James <bvlyqt@gmail.com>
2023-05-09 15:59:55 +0900

    Minor update to Lualine, adding character count counter when in Visual mode.

--- lua/plugins/lazy-lualine.lua
+++ lua/plugins/lazy-lualine.lua
@@ -13,12 +13,12 @@ return {
             theme = "kanagawa",
         },
         sections = {
-            lualine_a = {{"mode", fmt = function(str) return str:sub(1,1) end}}, -- Display the mode as a single character.
-            lualine_b = {{"diagnostics"}},
-            lualine_c = {{"filename", file_status = true, newfile_status = true, path = 1, shorting_target = 10, symbols = {modified = "MODIFIED", readonly = "READ", unnamed = "UN", newfile = "NF"}}},
+            lualine_a = {{ "mode", fmt = function(str) return str:sub(1,1) end }, },
+            lualine_b = {{ "diagnostics" }},
+            lualine_c = {{ "filename", file_status = true, newfile_status = true, path = 1, shorting_target = 10, symbols = { modified = "MODIFIED", readonly = "READ", unnamed = "UN", newfile = "NF" },},},
             lualine_x = {},
-            lualine_y = {},
-            lualine_z = {{"location"}},
+            lualine_y = {{ function() local starts = vim.fn.line("v") local ends = vim.fn.line(".") local count = starts <= ends and ends - starts + 1 or starts - ends + 1 local wc = vim.fn.wordcount() return wc["visual_chars"] end, cond = function() return vim.fn.mode():find("[Vv]") ~= nil end,},},
+            lualine_z = {{ "location" }},
        },
     },
     config = function(_, opts)
