0dbc7a0620b96c6fbd05c5baf2c4d3a2b70dd8a4
Christopher James <bvlyqt@gmail.com>
2023-08-29 19:32:24 +0900

    Misc. update
    Added better error logging to the setup failure case.

--- lua/plugins/lazy-cmp-nvim-lsp.lua
+++ lua/plugins/lazy-cmp-nvim-lsp.lua
@@ -6,11 +6,9 @@ return {
     enable = true,
     lazy = true,
     config = function()
-        cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+        local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 		if not cmp_nvim_lsp_ok then
 			return
 		end
-        -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-        -- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
     end,
 }
