d8ae7668294b3f72817ed40bdf2e22854bc7f03d
Christopher James <bvlyqt@gmail.com>
2023-08-29 19:24:16 +0900

    Log setup failure
    The plugin will now say something when setup fails.

--- lua/plugins/lazy-mason-lspconfig.lua
+++ lua/plugins/lazy-mason-lspconfig.lua
@@ -20,6 +20,7 @@ return {
     config = function()
 		local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 		if not mason_lspconfig_ok then
+            vim.notify(vim.inspect(mason_lspconfig), vim.log.levels.ERROR)
 			return
 		end
         mason_lspconfig.setup()
