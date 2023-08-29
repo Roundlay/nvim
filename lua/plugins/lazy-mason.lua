dfad1f56f2889345b5e697b8e0f38711e39a1aa1
Christopher James <bvlyqt@gmail.com>
2023-08-29 19:22:35 +0900

    Misc. cleanup
    Removed unneeded dependencies, keys, and lazy-load event parameters, and added better setup failure logging.

--- lua/plugins/lazy-mason.lua
+++ lua/plugins/lazy-mason.lua
@@ -1,30 +1,22 @@
 -- mason.nvim
 
--- Incomming dependencies: nvim-lspconfig
--- Outgoing dependencies: mason-lspconfig
-
 return {
     "williamboman/mason.nvim",
     -- name  = "mason",
     build = ":MasonUpdate",
-    lazy  = true,
-    event = "BufReadPre",
-    cmd   = "Mason",
-    keys = {
-        -- { "<leader>mm", "<cmd>Mason<CR>", desc = "Toggle Mason Menu" },
-    },
+    lazy = true,
+    cmd = "Mason",
     opts = {
         ui = {
             border = "none",
         },
     },
     config = function(_, opts)
-        -- NOTE You were experimenting with ability to hot-load plugins?
 		local mason_ok, mason = pcall(require, "mason")
 		if not mason_ok then
+            vim.notify(vim.inspect(mason), vim.log.levels.ERROR)
 			return
 		end
         mason.setup(opts)
-        -- require("mason").setup(opts)
     end,
 }
