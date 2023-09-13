e44525e9645c2473cec7a2dd48648e0ab35e2688
Christopher James <bvlyqt@gmail.com>
2023-09-13 11:56:03 +0900

    Minor update to Tree-sitter highlights
    Added the Tree-sitter capture group for curly braces for enums to the list of custom highlights.

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -2,34 +2,36 @@
 
 -- local fuji_white = "#DCD7BA"
 local sumi_ink_2 = "#2A2A37"
+local winter_blue = "#252535"
+local sumiInk3 = "#363646"
+local sumiInk4 = "#54546D"
 
 -- NOTE: It's so stupid that I have to do this.
 NONE = NONE
 
 vim.api.nvim_set_hl(0, "LineNr", {fg = sumi_ink_2, bg = NONE, gui = NONE})
 
--- TODO Highlight doesn't apply when set with vim.api.nvim_set_hl...
-
 vim.cmd [[ highlight WinSeparator guifg=bg guibg=bg cterm=NONE ]]
 
-vim.api.nvim_set_hl(0, "@procOpeningBrace",        {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@procClosingBrace",        {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@forOpeningBrace",         {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@forClosingBrace",         {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@forAssignmentSemicolon",  {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@ifStatementOpeningBrace", {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@ifStatementClosingBrace", {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@elseClauseOpeningBrace",  {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@elseClauseClosingBrace",  {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@forStatementSemicolon",   {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@switchOpeningBrace",      {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@switchClosingBrace",      {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@structOpeningBrace",      {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@structClosingBrace",      {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@foreignOpeningBrace",     {fg = sumi_ink_2})
-vim.api.nvim_set_hl(0, "@foreignClosingBrace",     {fg = sumi_ink_2})
-
--- TODO: Figure out a way to check which theme is currently active so that
--- you can set the indent blankline colours to match dynamically.
-
 vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg = "#252535"}) -- Kanagawa
+
+vim.api.nvim_set_hl(0, "@curlybraces", {fg = sumiInk3})
+
+-- vim.api.nvim_set_hl(0, "@procOpeningBrace",        {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@procClosingBrace",        {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@forOpeningBrace",         {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@forClosingBrace",         {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@forAssignmentSemicolon",  {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@ifStatementOpeningBrace", {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@ifStatementClosingBrace", {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@elseClauseOpeningBrace",  {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@elseClauseClosingBrace",  {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@forStatementSemicolon",   {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@switchOpeningBrace",      {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@switchClosingBrace",      {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@structOpeningBrace",      {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@structClosingBrace",      {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@enumOpeningBrace",        {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@enumClosingBrace",        {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@foreignOpeningBrace",     {fg = sumiInk3})
+-- vim.api.nvim_set_hl(0, "@foreignClosingBrace",     {fg = sumiInk3})
