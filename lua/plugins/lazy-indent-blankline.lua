95e8d1ba18beb9ba482bbd32c96d6b6af674f5c8
Christopher James <bvlyqt@gmail.com>
2023-10-11 14:33:14 +0900

    Update lazy-indent-blankline.lua
    Updated the indent-blankline.nvim plugin to bring it up to date with v3.0.0. Additionally added an example of how to implement differing indent-line characters based on line type, e.g. dotted lines on empty lines, solid ones otherwise, etc.

--- lua/plugins/lazy-indent-blankline.lua
+++ lua/plugins/lazy-indent-blankline.lua
@@ -3,22 +3,62 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
     -- name = "indent-blankline",
+    main = "ibl", -- Required after v3.0.0
     enabled = true,
     lazy = true,
     event = {"BufReadPost", "BufNewFile"},
-    config = function ()
-        require("indent_blankline").setup {
+    opts = {
+        indent = {
             char = "┃",
-            char_blankline = "┋",
-            show_current_context = false,
-            show_current_context_start = false,
-            show_end_of_line = true,
-            show_first_indent_level = true,
-            show_foldtext = true,
-            show_trailing_blankline_indent = true,
-            strict_tabs = false,
-            use_treesitter = false, -- Was causing some issues with .odin files.
-            use_treesitter_scope = true,
-        }
-    end,
+            smart_indent_cap = true,
+            highlight = {
+                "winter_blue",
+            },
+        },
+        scope = {
+            enabled = false
+        },
+        viewport_buffer = {
+            min = 100,
+            max = 800,
+        },
+    },
+    config = function(_, opts)
+        local hooks = require("ibl.hooks")
+
+        -- This changes the indentation line colour to Kanagawa's Winter Blue.
+        hooks.register(
+            hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "winter_blue", { fg = "#252535" })
+            end
+        )
+
+        -- This changes the indentation character to "┋" when line is empty.
+        -- Performance impact unclear, but keep in mind that we're iterating
+        -- over all virtual text whenever the cursor moves. This is probably
+        -- how earlier versions of the plugin did it anyway.
+        -- hooks.register(
+        --     hooks.type.VIRTUAL_TEXT, function(_, bufnr, row, virtual_text)
+        --         local config = require("ibl.config").get_config(bufnr)
+        --         local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false)[1]
+        --         if line == "" then
+        --             for _, text in ipairs(virtual_text) do
+        --                 if text[1] == config.indent.char then text [1] = "┋"
+        --                 -- if text[1] == config.indent.char then text [1] = "╋"
+        --                 end
+        --             end
+        --         end
+        --         return virtual_text
+        --     end
+        -- )
+
+        local ibl_ok, ibl = pcall(require, "ibl")
+
+        if not ibl_ok then
+            print("Error loading 'indent-blankline'.")
+            return
+        end
+
+        ibl.setup(opts)
+    end
 }
