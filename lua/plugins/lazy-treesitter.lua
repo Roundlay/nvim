2c9b1f72db43922ab6562cb02d9dff8afd63e151
Christopher James <bvlyqt@gmail.com>
2024-02-17 10:40:45 +0900

    New events and incremental selection
    Added "BufNewFile" to the event list triggering Treesitter, included "markdown" in the list of supported languages, and introduced incremental selection with custom key mappings. Also corrected the module name in the error message and modified the setup call to use the aliased 'nvim_treesitter' variable.

--- lua/plugins/lazy-treesitter.lua
+++ lua/plugins/lazy-treesitter.lua
@@ -7,7 +7,10 @@ return {
     version = false,
     build = ":TSUpdateSync",
     lazy = true,
-    event = "BufReadPost",
+    event = {
+        "BufReadPost",
+        "BufNewFile",
+    },
     cmd = {
         "TSEnable",
     },
@@ -21,6 +24,7 @@ return {
             "cmake",
             "query",
             "python",
+            "markdown",
         },
         sync_install = false,
         auto_install = false,
@@ -33,6 +37,15 @@ return {
         },
         indent = {
             enable = false -- Experimental and broke indentation in Odin.
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "zz",
+                node_incremental = "zn",
+                scope_incremental = "zi",
+                node_decremental = "zx",
+            },
         },
         playground = {
             enable = true,
@@ -54,11 +67,12 @@ return {
         },
     },
     config = function(_, opts)
-        local nvim_treesitter_ok, nvim_treesitter = pcall(require, "nvim-treesitter")
+        local nvim_treesitter_ok, nvim_treesitter = pcall(require, "nvim-treesitter.configs")
         if not nvim_treesitter_ok then
-            print("Error loading 'nvim_treesitter'.")
+            print("Error loading `nvim_treesitter`.")
             return
         end
-        require("nvim-treesitter.configs").setup(opts)
+        -- require("nvim-treesitter.configs").setup(opts)
+        nvim_treesitter.setup(opts)
     end
 }
