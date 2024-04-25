00fc53dad1cb2b4493f44267597c6c8ecc13ed5d
Christopher James <bvlyqt@gmail.com>
2024-04-25 10:11:25 +0900

    Added highlighting for Leap search

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -5,10 +5,11 @@ if vim.g.vscode then
 end
 
 -- local fuji_white = "#DCD7BA"
-local sumi_ink_2 = "#2A2A37"
--- local winter_blue = "#252535"
+local sumiInk2 = "#2A2A37"
+local winterBlue = "#252535"
 local sumiInk3 = "#363646"
--- local sumiInk4 = "#54546D"
+local sumiInk4 = "#54546D"
+
 -- local boatYellow = "#938056"
 -- local autumnYellow = "#DCA561"
 -- local autumnRed = "#C34043"
@@ -18,10 +19,32 @@ local sumiInk3 = "#363646"
 -- NOTE: It's so stupid that I have to do this.
 NONE = NONE
 
-vim.api.nvim_set_hl(0, "LineNr", {fg = sumi_ink_2, bg = NONE, gui = NONE})
-vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg = "#252535"}) -- Kanagawa
+vim.api.nvim_set_hl(0, 'LeapBackdrop', { fg = sumiInk4, bg = NONE, gui = NONE })
+
+vim.api.nvim_set_hl(0, "LineNr", {fg = sumiInk2, bg = NONE, gui = NONE})
+
+vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg = winterBlue})
+
 vim.api.nvim_set_hl(0, "@curlybraces", {fg = sumiInk3})
+
+vim.cmd [[ highlight NonText cterm=NONE ctermfg=NONE ]] -- See term.txt in the docs
 vim.cmd [[ highlight WinSeparator guifg=bg guibg=bg cterm=NONE ]]
+
+-- TODO: Can highlight be put in an autocmd so that they are applied when the colorscheme changes?
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--   callback = function ()
+--     -- NOTE: It's so stupid that I have to do this.
+--     NONE = NONE
+--     vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+--     -- vim.api.nvim_set_hl(0, 'LeapMatch', { link = 'Comment' })
+--     vim.api.nvim_set_hl(0, 'LeapMatch', { fg = autumnYellow, bg = NONE, gui = NONE })
+--     vim.api.nvim_set_hl(0, "LineNr", {fg = sumi_ink_2, bg = NONE, gui = NONE})
+--     vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg = "#252535"}) -- Kanagawa
+--     vim.api.nvim_set_hl(0, "@curlybraces", {fg = sumiInk3})
+--     vim.cmd [[ highlight NonText cterm=NONE ctermfg=NONE ]] -- See term.txt in the docs
+--     vim.cmd [[ highlight WinSeparator guifg=bg guibg=bg cterm=NONE ]]
+--   end
+-- })
 
 -- vim.api.nvim_set_hl(0, "@procOpeningBrace",        {fg = sumiInk3})
 -- vim.api.nvim_set_hl(0, "@procClosingBrace",        {fg = sumiInk3})
