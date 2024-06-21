4f1bee0ef5e9dc06dfc6ea5046b5deec591f876c
Christopher James <bvlyqt@gmail.com>
2024-06-21 12:17:51 +0900

    Added helpful comment
    Added a helpful comment that explains how the acceleration table works.

--- lua/plugins/lazy-accelerated-jk.lua
+++ lua/plugins/lazy-accelerated-jk.lua
@@ -2,21 +2,32 @@
 
 return {
     "rainbowhxch/accelerated-jk.nvim",
-    -- name = "accelerated-jk",
     enabled = false,
     lazy = true,
-    event = {"BufReadPost", "InsertEnter"},
     keys = {
-        {"j", "<Plug>(accelerated_jk_j)", desc = "Accelerated j"},
-        {"k", "<Plug>(accelerated_jk_k)", desc = "Accelerated k"},
+        {"j", "<Plug>(accelerated_jk_j)", desc = "Accelerated <j>"},
+        {"k", "<Plug>(accelerated_jk_k)", desc = "Accelerated <k>"},
     },
     opts = {
         mode = "time_driven",
         enable_deceleration = true,
-        acceleration_table = {12,18,24,30},
+        acceleration_limit = 150,
+        -- Each value in the acceleration table represents the number of
+        -- <j> or <k> inputs required to increase the acceleration multiple by the
+        -- index of the value in the table. E.g. for {5, 10, 20} the acceleration
+        -- multiples are {1x, 2x, 3x}, and so on. The accceleration multiple will
+        -- be 1x until the equivalent of 5*<j> inputs have been received, 2x when
+        -- the equivalent of 5-10 <j> inputs have been received, and 3x when the
+        -- equivalent of 10-20 <j> inputs have been received.
+        acceleration_table = {12, 24, 48},
         deceleration_table = {{50, 200}},
     },
     config = function(_, opts)
-        require("accelerated-jk").setup(opts)
+		local acceleratedjk_ok, accelerated_jk = pcall(require, "accelerated-jk")
+		if not acceleratedjk_ok then
+            vim.notify(vim.inspect(accelerated_jk), vim.log.levels.ERROR)
+			return
+		end
+        accelerated_jk.setup(opts)
     end,
 }
