461623845728709e7713d17303defd8616fd7002
Christopher James <bvlyqt@gmail.com>
2024-08-17 12:42:49 +0900

    Enable line wrapping and update cursorline settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -66,7 +66,7 @@ vim.api.nvim_set_option('mouse', 'a') -- Enables mouse scrolling in 'a'll modes.
 -- Line-wrap
 
 vim.o.linebreak = true -- Wrap text on word boundaries.
-vim.o.wrap = false -- Wrap lines. 
+vim.o.wrap = true -- Wrap lines. 
 
 -- Syntax
 
@@ -82,6 +82,7 @@ vim.o.list = false
 
 vim.o.termguicolors = true -- Enable true 24bit colour support; use 'gui' instead of 'cterm' in highlights.
 vim.o.background = 'dark'
+vim.cmd [[ colorscheme off ]]
 
 -- Status Line
 
@@ -91,17 +92,17 @@ vim.o.showmode = false -- Don't show the command line mode (e.g. '-- INSERT --')
 
 -- Line/Column
 
-vim.o.cursorline = false -- Highlight the current line. Known to cause performance issues.
--- vim.o.rnu = true
--- vim.o.numberwidth = 3
+vim.o.cursorline = true -- Highlight the current line. Known to cause performance issues.
+vim.o.rnu = true
+vim.o.numberwidth = 2
 -- vim.opt.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . '  ' : v:lnum) : ''}%=%s"
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 vim.o.fillchars = 'eob: ' -- This needs to be called after 'laststatus'. Remove empty buffer symbols.
 vim.o.signcolumn = 'no' -- Force the signcolumn to remain hidden.
 
 -- Search
 
-vim.o.synmaxcol = 256 -- Max columns to search for syntax items; 0 = infinity.
+vim.o.synmaxcol = 0 -- Max columns to search for syntax items; 0 = infinity.
 vim.o.incsearch = true -- Show search results as you type.
 vim.o.hlsearch = true -- Highlight search results.
 vim.o.showmatch  = true -- Highlight matching braces, etc.
@@ -123,8 +124,8 @@ vim.o.breakindent = true -- Visually indent wrapped lines.
 -- Timing
 
 vim.g.redrawtime = 100 -- Time in ms to redraw the screen. (Default: 2000)
-vim.o.updatetime = 4000 -- Write swap-file to disk every `updatetime` ms.
-vim.o.timeoutlen = 500 -- Time in ms to wait for mapped sequences to complete.
+vim.o.updatetime = 1000 -- Write swap-file to disk every `updatetime` ms.
+vim.o.timeoutlen = 300 -- Time in ms to wait for mapped sequences to complete.
 vim.o.ttimeoutlen = 0 -- Time in ms to wait for key-code sequences to complete.
 
 -- Folds
@@ -170,6 +171,13 @@ vim.diagnostic.config({ underline = false })
 vim.cmd [[ autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o ]] -- Disable auto comment formatting. (`c`: auto-wrap comments; `r`: auto-insert comment leader after hitting <Enter> in Insert mode; `o`: Auto-insert the comment leader after hitting 'o' or 'O' in Normal mode.
 vim.cmd [[ autocmd BufWinEnter,WinEnter term://* startinsert ]] -- Disable auto comment formatting. (`c`: auto-wrap comments; `r`: auto-insert comment leader after hitting <Enter> in Insert mode; `o`: Auto-insert the comment leader after hitting 'o' or 'O' in Normal mode.
 vim.cmd [[ autocmd BufLeave term://* stopinsert]] -- Disable auto comment formatting. (`c`: auto-wrap comments; `r`: auto-insert comment leader after hitting <Enter> in Insert mode; `o`: Auto-insert the comment leader after hitting 'o' or 'O' in Normal mode.
+
+-- This activates cowboy mode, which complains when you spam movement keys.
+-- local discipline = require("scripts")
+-- discipline.cowboy()
+
+-- ARCHIVE
+
 -- vim.cmd [[ au BufNewFile,BufRead *.odin map=<C-P> :w<Return>:%!odinfmt %<Return> ]]
 -- vim.cmd [[ au BufNewFile,BufRead *.odin set syntax=odin ]] -- Set Odin syntax highlighting for .odin files.
 -- vim.cmd [[ au BufNewFile,BufRead *.odin set filetype=odin ]] -- Set Odin filetype for .odin files. (Handled by filetype plugin.)
